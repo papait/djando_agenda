25d742ee7d4688069d460fc98211b3c12a5bef94
papait <leonardovareschi@gmail.com>
2025-10-12 22:39:10 -0300

    create models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','firts_name','last_name','phone','email')
+    ordering = ('id',)
+    #list_filter = ('create_date',)
+    list_display_links = ('firts_name',)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Contact (models.Model):
+    firts_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    create_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.firts_name} {self.last_name}'
