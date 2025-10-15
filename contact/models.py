d826cbbe8197422471d8efbf120caefbc84a872a
papait <leonardovareschi@gmail.com>
2025-10-14 23:29:46 -0300

    Create View and CCS sTYLE

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category (models.Model):
@@ -25,6 +26,11 @@ class Contact (models.Model):
     category = models.ForeignKey(
         Category, 
         on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
+    owner =  models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, #CASCADE APAGA TUDO
         blank=True, null=True
         )
 
---
+++ contact/views/__init__.py
@@ -0,0 +1,2 @@
+# Criando um package do python pra gerenciar varias view, django vai ler esse pacote como se fosse um modulo view
+from contact.views.contact_views import *
