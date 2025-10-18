2581719829c1dd9ff9ee48b69d293d817365eadf
papait <leonardovareschi@gmail.com>
2025-10-18 16:31:29 -0300

    Make HTML and Headers

--- contact/admin.py
+++ contact/admin.py
@@ -4,11 +4,11 @@ from contact.models import Contact, Category
 # Register your models here.
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','firts_name','last_name','phone','email')
-    ordering = ('id',)
+    list_display = ('id','firts_name','last_name','phone','email','show')
+    ordering = ('-id',)
     #list_filter = ('create_date',)
     list_display_links = ('firts_name',)
-
+    list_editable = ('show',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from contact.models import Contact
+from django.http import Http404
 
 def index(request):
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')[:10]
+
+    #print(contacts.query)
+
+    context = {
+        'contacts' : contacts,
+        'site_title' : "Contatos"
+    }
+
     return render(
         request,
-        'contact/index.html'
+        'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+
+    #singles_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact =  get_object_or_404(Contact, 
+                        pk=contact_id, show=True)
+    #if singles_contact is None:
+    #    raise Http404()
+    #print(contacts.query)
+    contact_name = f'{single_contact.firts_name} {single_contact.last_name} - '
+
+
+    context = {
+        'contact' : single_contact,
+        'site_title' : contact_name
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
     )
