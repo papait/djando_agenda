b745e97dfd499a65ee2ce3314891d1a24e7d926f
papait <leonardovareschi@gmail.com>
2025-10-19 00:16:35 -0300

    Create paginated

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.http import Http404
+from django.db.models import Q
+from django.http import HttpRequest
+from django.core.paginator import Paginator
 
-def index(request):
+
+def index(request :HttpRequest):
 
     contacts = Contact.objects\
         .filter(show=True)\
-        .order_by('-id')[:10]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj =  paginator.get_page(page_number)
 
-    #print(contacts.query)
+    print(page_obj)
 
     context = {
-        'contacts' : contacts,
+        'page_obj' : page_obj,
         'site_title' : "Contatos"
     }
 
@@ -21,7 +29,7 @@ def index(request):
         context
     )
 
-def contact(request, contact_id):
+def contact(request :HttpRequest, contact_id):
 
     #singles_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact =  get_object_or_404(Contact, 
@@ -40,5 +48,36 @@ def contact(request, contact_id):
     return render(
         request,
         'contact/contact.html',
+        context
+    )
+
+def search(request :HttpRequest):
+    search_value = request.GET.get('q','').strip() #Name do form no header
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(Q(firts_name__icontains=search_value)  |
+                Q(last_name__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj =  paginator.get_page(page_number)
+
+    #print(contacts.query) Pra ver a consulta
+
+    context = {
+        'page_obj' : page_obj,
+        'site_title' : "Contatos",
+        'search_value' : search_value
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context
     )
